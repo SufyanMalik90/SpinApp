c7bd6d5994f409ca06d0ca4ccdd510610195b1a4
sufyanmalik90 <shaikh.arman1239090@gmail.com>
2023-07-28 16:12:48 +0500

    add-firebase-auth

--- src/Screens/SignInScreen.js
+++ src/Screens/SignInScreen.js
@@ -1,176 +1,164 @@
 import { 
-    View, 
-    Text,
-    StyleSheet,
-    TextInput,
-    Image,
-    useWindowDimensions,
-    ScrollView,
-    Keyboard,
-    Alert
-} from 'react-native'
+  View, 
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  useWindowDimensions,
+  ScrollView,
+  Keyboard,
+  Alert,
+  SafeAreaView
+} from 'react-native';
 import React, { useState } from 'react';
-import CustomButton from '../Components/CustomButton'
-import Logo from '../../assets/image/logo.png'
-import Input from './Input'
+import CustomButton from '../Components/CustomButton';
+import Logo from '../../assets/image/logo.png';
+import Input from './Input';
 import Loader from './Loader';
-// import { useNavigation } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth'; // Import Firebase Auth
 
-const SignInScreen = ({navigation}) => {
+const SignInScreen = ({ navigation }) => {
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState({});
+  const [loading, setLoading] = useState(false);
 
-    const [input, setInput] = useState({
-      email: '',
-      password: '',
+  const Validate = () => {
+    Keyboard.dismiss(); // hide the keyboard
+    let isValid = true;
+    const errors = {};
 
-    });
-    const [error, setError] = useState({});
-    const [loading, setLoading] = useState(false);
+    if (!input.email) {
+      errors.email = 'Please Input Email';
+      isValid = false;
+    } else if (!input.email.match(/\S+@\S+\.\S+/)) {
+      errors.email = 'Please Input Valid Email';
+      isValid = false;
+    }
 
+    if (!input.password) {
+      errors.password = 'Please Input Password';
+      isValid = false;
+    } else if (input.password.length < 6) {
+      errors.password = 'Minimum Length of Password is 6';
+      isValid = false;
+    }
 
-    const Validate = () => {
-      Keyboard.dismiss(); // hide the keyboard
-      let Valid = true;
-      if (!input.email) {
-        handleError('Please Input Email', 'email');
-        Valid = false;
-      } else if (!input.email.match(/\S+@\S+\.\S+/)) {
-        handleError('Please Input Valid Email', 'email');
-        Valid = false;
-      }
-  
-      if (!input.password) {
-        handleError('Please Input Password', 'password');
-      } else if (input.password.length < 6) {     // minimum length for password is 6
-        handleError('Minimum Length of Password is 6', 'password');
-      }
-  
-      if (Valid) {
-        register();
-      }
-  
-    };
+    setError(errors);
+    return isValid;
+  };
 
-    let loginUser = () => {
-      //Firebase connect code
-    };
+  const loginUser = async () => {
+    try {
+      const { email, password } = input;
+      await auth().signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log('Error logging in:', error);
+      throw new Error('Invalid email or password.');
+    }
+  };
 
-    const register = () => {
-      setLoading(true);
-      setTimeout(() => {
+  const handleOnChange = (text, input) => {
+    setInput((prevState) => ({ ...prevState, [input]: text }));
+  };
+
+  const { height } = useWindowDimensions();
+
+  const onSignUpPressed = () => {
+    navigation.navigate('SignUp');
+  };
+
+  const onLoginPressed = async () => {
+    const isValid = Validate();
+    if (isValid) {
+      try {
+        setLoading(true);
+        await loginUser();
         setLoading(false);
-  
-  
-        try {
-  
-          loginUser();
-          Alert.alert('Welcome', 'To Spin App');
-          navigation.replace('Home');
-          // console.log(`Username is: ${input.username} Email: ${input.email} Password: ${input.password} Phone: ${input.phone}`);
-  
-        } catch (error) {
-          Alert.alert('Error', 'Something Went Wrong..!!');
-  
-        }
-  
-      }, 2000)
+        Alert.alert('Welcome', 'To Spin App');
+        navigation.replace('Home');
+      } catch (error) {
+        setLoading(false);
+        Alert.alert('Error', error.message);
+      }
+    } else {
+      Alert.alert('Error', 'Please fill in all fields correctly.');
     }
-    const handleOnChange = (text, input) => {
-      setInput((prevState) => ({ ...prevState, [input]: text }));  // get text from user & set to input fields  
-    };
-    const handleError = (errorMessage, input) => {
-      setError((prevState) => ({ ...prevState, [input]: errorMessage }));  // clear error message & back to previous state  
-    };
-    const { height } = useWindowDimensions();
-    // const navigation = useNavigation();
-
-    const onSignUpPresssed = () => {
-      // console.warn("Sign Up");
-      navigation.navigate('SignUp');
-    };
-
-    const onLoginPressed = () => {
-      Validate();
-    }
+  };
 
   return (
-    
-      <View style={styles.root}>
+    <SafeAreaView style={styles.root}>
+     
         <Loader visible={loading} />
-          <Image source={Logo} style={[styles.logo, { height: height * 0.3 }]} resizeMode="contain" />
-          <View style={styles.loginContainer}>
-              <Text style={styles.textLogin}>Login your account</Text>
+        <Image source={Logo} style={[styles.logo, { height: height * 0.3 }]} resizeMode="contain" />
+        <View style={styles.loginContainer}>
+          <Text style={styles.textLogin}>Login your account</Text>
 
-              
-                <Input
-                placeholder='Email'
-                iconName='email-outline'
-                error={error.email}
-                onFocus={() => {
-                  handleError(null, 'email');
-                }}
-                onChangeText={(text) => handleOnChange(text, "email")}
-              />
+          <Input
+            placeholder='Email'
+            iconName='email-outline'
+            error={error.email}
+            onFocus={() => {
+              setError((prevState) => ({ ...prevState, email: null }));
+            }}
+            onChangeText={(text) => handleOnChange(text, 'email')}
+          />
 
-              <Input
-                placeholder='Enter Your Password'
-                iconName='lock-outline'
-                password
-                error={error.password}
-                onFocus={() => {
-                  handleError(null, 'password');
-                }}
+          <Input
+            placeholder='Enter Your Password'
+            iconName='lock-outline'
+            password
+            error={error.password}
+            onFocus={() => {
+              setError((prevState) => ({ ...prevState, password: null }));
+            }}
+            onChangeText={(text) => handleOnChange(text, 'password')}
+          />
 
-                onChangeText={(text) => handleOnChange(text, "password")}
-              />
-
-              <CustomButton text='Login' onPress={onLoginPressed}/>
-              <View style={styles.message}>
-              <Text>Create a new account? <Text style={styles.textInput} onPress={onSignUpPresssed}>SignUp</Text></Text>
-              </View>
+          <CustomButton text='Login' onPress={onLoginPressed} />
+          <View style={styles.message}>
+            <Text>Create a new account? <Text style={styles.textInput} onPress={onSignUpPressed}>SignUp</Text></Text>
           </View>
-      </View>
-  )
-}
+        </View>
+      
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    root : {
-        flex : 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#6238b4',
-      },
-      loginContainer : {
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        width: '75%',
-        height: 'auto',
-        borderRadius: 10,
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#6238b4',
+  },
+  loginContainer: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    width: '75%',
+    height: 'auto',
+    borderRadius: 10,
+  },
+  textLogin: {
+    color: '#6238b4',
+    marginTop: 5,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  message: {
+    marginBottom: 10,
+  },
+  textInput: {
+    color: '#02074c',
+  },
+  logo: {
+    width: '70%',
+    maxWidth: 300,
+    maxHeight: 150,
+    marginBottom: 10,
+  }
+});
 
-      },
-      textLogin: {
-        color: '#6238b4',
-        marginTop: 5,
-        fontSize: 18,
-        fontWeight: 'bold',
-      },
-      message:{
-        marginBottom: 10
-      },
-      textInput :{
-        // width: '90%',
-        // height: 35,
-        // backgroundColor: '#bdaee7',
-        // borderRadius: 10,
-        // fontSize: 13,
-        color: '#02074c'
-        
-      },
-    logo: {
-      width: '70%',
-      maxWidth: 300,
-      maxHeight: 150,
-      marginBottom: 10,
-    }
-  })
-
-export default SignInScreen
+export default SignInScreen;
--- src/Screens/SignUpScreen.js
+++ src/Screens/SignUpScreen.js
@@ -1,114 +1,118 @@
-import { 
-    View, 
-    Text,
-    StyleSheet,
-    TextInput,
-    Image,
-    useWindowDimensions,
-    ScrollView,
-    Keyboard,
-    Alert
-} from 'react-native'
-import React, {useState} from 'react';
-import CustomButton from '../Components/CustomButton'
-import Logo from '../../assets/image/logo.png'
-import Input from './Input'
+import { View, Text, StyleSheet, TextInput, Image, useWindowDimensions, ScrollView, Keyboard, Alert } from 'react-native';
+import React, { useState } from 'react';
+import CustomButton from '../Components/CustomButton';
+import CountryPicker from 'react-native-country-picker-modal';
+import Logo from '../../assets/image/SigmaAdsdlogo.png';
+import Input from './Input';
 import Loader from './Loader';
+import auth from '@react-native-firebase/auth';
 
-const SignUpScreen = ({navigation}) => {
-
+const SignUpScreen = ({ navigation }) => {
   const [input, setInput] = useState({
     email: '',
-    username : '',
-    password : '',
-    phone    : '',
-
+    username: '',
+    password: '',
+    phone: '',
   });
+  
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({});
+  const [countryCode, setCountryCode] = useState('PK');
+  const [callingCode, setCallingCode] = useState('92');
+  
+  const handleConfirmPasswordChange = (text) => {
+    setConfirmPassword(text);
+  };
 
-  const [loading,setLoading] = useState(false);
-    const [error, setError] = useState({});
-
-    const Validate = () => {
-
-      Keyboard.dismiss(); // hide the keyboard
-      let Valid = true;
-      if(!input.email){
-        handleError('Please Input Email', 'email');
-        Valid = false;
-        } else if(!input.email.match(/\S+@\S+\.\S+/)) {
-        handleError('Please Input Valid Email', 'email');
-        }
-      if(!input.username){
-          handleError('Please Input Username', 'username');
-          Valid = false;  
-      };
-      if(!input.password){
-        handleError('Please Input Password', 'password'); 
-      } else if(input.password.length < 6){     // minimum length for password is 6
-        handleError('Minimum Length of Password is 5', 'password'); 
-      } 
-      if(!input.phone){
-        handleError('Please Input Phone', 'phone'); 
-  } else if(input.phone.length < 10){     // minimum length for address is 6
-    handleError('Minimum Length of Phone is 10', 'phone'); 
-  } 
-      if(Valid){
-        register();
-      }
-
-    };
-
-    let createUser = () => {
-      //Firebase Register User
-    };
-
-    const register = () =>{
-      setLoading(true);
-      setTimeout(() =>{
-        setLoading(false);
-        // createUser();
-        
-        try {
-         
-         createUser();
-         Alert.alert('Sucessful','Record Inserted');
-         navigation.navigate('SignIn');
-        // console.log(`Username is: ${input.username} Email: ${input.email} Password: ${input.password} Phone: ${input.phone}`);
-          
-        } catch (error) {
-          Alert.alert('Error', 'Something Went Wrong..!!');
-          
-        }
-
-      }, 2000)
+  const Validate = () => {
+    Keyboard.dismiss(); // hide the keyboard
+    let Valid = true;
+    if (!input.email) {
+      handleError('Please Input Email', 'email');
+      Valid = false;
+    } else if (!input.email.match(/\S+@\S+\.\S+/)) {
+      handleError('Please Input Valid Email', 'email');
     }
+    if (!input.username) {
+      handleError('Please Input Username', 'username');
+      Valid = false;
+    }
+    if (!input.password) {
+      handleError('Please Input Password', 'password');
+    } else if (input.password.length < 6) {
+      // minimum length for password is 6
+      handleError('Minimum Length of Password is 5', 'password');
+    }
+    if (input.password !== confirmPassword) {
+      // handleError('Passwords do not match.', 'password');
+      handleError('Passwords do not match.', 'confirmPassword');
+      return;
+    }
+    else if(!confirmPassword){
+      handleError('Please Input Confirm Password', 'confirmPassword');
+    }
+    if (!input.phone) {
+      handleError('Please Input Phone', 'phone');
+    } else if (input.phone.length < 10) {
+      // minimum length for phone is 10
+      handleError('Minimum Length of Phone is 10', 'phone');
+    }
+    if (Valid) {
+      register();
+    }
+  };
 
-    const handleOnChange = (text,input) => {
-      setInput((prevState)=>({...prevState,  [input]: text}));  // get text from user & set to input fields  
-    };
-    const handleError = (errorMessage, input) => {
-      setError((prevState)=>({...prevState,  [input]: errorMessage}));  // clear error message & back to previous state  
-    };
-    
-    const onRegesterPresssed = () => {
-      //console.warn("Sign Up");
-      Validate();
-    };
+  const createUser = async () => {
+    try {
+      const { email, password } = input;
+      await auth().createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log('Error creating user:', error);
+      throw new Error('Something went wrong during user registration.');
+    }
+  };
 
-    const { height } = useWindowDimensions();
-
-    const onSignInPresssed = () => {
-      // console.warn("Sign Up");
+  const register = async () => {
+    setLoading(true);
+    try {
+      await createUser();
+      setLoading(false);
+      Alert.alert('Successful', 'Record Inserted');
       navigation.navigate('SignIn');
-    };
+    } catch (error) {
+      setLoading(false);
+      Alert.alert('Error', error.message);
+    }
+  };
+
+  const handleOnChange = (text, input) => {
+    setInput((prevState) => ({ ...prevState, [input]: text })); // get text from user & set to input fields
+  };
+
+  const handleError = (errorMessage, input) => {
+    setError((prevState) => ({ ...prevState, [input]: errorMessage })); // clear error message & back to previous state
+  };
+
+  const onRegesterPresssed = () => {
+    //console.warn("Sign Up");
+    Validate();
+  };
+
+  const { height } = useWindowDimensions();
+
+  const onSignInPresssed = () => {
+    // console.warn("Sign Up");
+    navigation.navigate('SignIn');
+  };
 
   return (
     <ScrollView>
       <View style={styles.root}>
-      <Loader visible={loading} />
-          <Image source={Logo} style={[styles.logo, { height: height * 0.3 }]} resizeMode="contain" />
-          <View style={styles.loginContainer}>
-              <Text style={styles.textLogin}>Create a new account</Text>
+        <Loader visible={loading} />
+        <Image source={Logo} style={[styles.logo, { height: height * 0.3 }]} resizeMode="contain" />
+        <View style={styles.loginContainer}>
+          <Text style={styles.textLogin}>Create a new account</Text>
 
           <Input
             placeholder='Username'
@@ -117,7 +121,7 @@ const SignUpScreen = ({navigation}) => {
             onFocus={() => {
               handleError(null, 'username');
             }}
-            onChangeText={(text) => handleOnChange(text, "username")}
+            onChangeText={(text) => handleOnChange(text, 'username')}
           />
           <Input
             placeholder='Email'
@@ -126,9 +130,9 @@ const SignUpScreen = ({navigation}) => {
             onFocus={() => {
               handleError(null, 'email');
             }}
-            onChangeText={(text) => handleOnChange(text, "email")}
+            onChangeText={(text) => handleOnChange(text, 'email')}
           />
-          <Input
+          {/* <Input
             keyboardType="numeric"
             placeholder='03XXXXXXXXX'
             iconName='phone'
@@ -136,8 +140,34 @@ const SignUpScreen = ({navigation}) => {
             onFocus={() => {
               handleError(null, 'phone');
             }}
-            onChangeText={(text) => handleOnChange(text, "phone")}
-          />  
+            onChangeText={(text) => handleOnChange(text, 'phone')}
+          />   */}
+          <View style={styles.inputContainer}>
+            <CountryPicker
+              countryCode={countryCode}
+              withFilter
+              withCurrencyButton={false}
+              withFlag
+              withCallingCode
+              onSelect={(country) => {
+                console.log('country', country);
+                const { cca2, callingCode } = country;
+                setCountryCode(cca2);
+                setCallingCode(callingCode[0]);
+              }}
+            />
+            <TextInput
+              style={{
+                color: '#000',
+                flex: 1,
+                fontSize: 12,
+              }}
+              placeholder='3XXXXXXXXX'
+              keyboardType='numeric'
+              error={error.phone}
+              onChangeText={(text) => handleOnChange(text, 'phone')}
+            />
+          </View>
           <Input
             placeholder='Password'
             iconName='lock-outline'
@@ -146,63 +176,75 @@ const SignUpScreen = ({navigation}) => {
               handleError(null, 'password');
             }}
             password
-            onChangeText={(text) => handleOnChange(text, "password")}
-          />   
-          <Input
-            placeholder='Confirm Password'
-            iconName='lock-outline'
-            error={error.password}
-            onFocus={() => {
-              handleError(null, 'password');
-            }}
-            password
-            onChangeText={(text) => handleOnChange(text, "password")}
-          />   
-
-              <CustomButton text='Register' onPress={onRegesterPresssed}/>
-              <View style={styles.message}>
-              <Text>I have already an account? <Text style={styles.textInput} onPress={onSignInPresssed}>Login</Text></Text>
-              </View>
+            onChangeText={(text) => handleOnChange(text, 'password')}
+          />
+            <Input
+          placeholder='Confirm Password'
+          iconName='lock-outline'
+          error={error.confirmPassword} // Update the error prop to show error for Confirm Password
+          onFocus={() => {
+            handleError(null, 'confirmPassword');
+          }}
+          password
+          onChangeText={(text) => {
+            handleOnChange(text, 'confirmPassword');
+            handleConfirmPasswordChange(text); // Add this line to update the Confirm Password state
+          }}
+        />
+          <CustomButton text='Register' onPress={onRegesterPresssed} />
+          <View style={styles.message}>
+            <Text>
+              I have already an account? <Text style={styles.textInput} onPress={onSignInPresssed}>Login</Text>
+            </Text>
           </View>
+        </View>
       </View>
-      </ScrollView>
-  )
-}
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    root : {
-        flex : 1,
-        // justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#6238b4',
-      },
-      loginContainer : {
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        width: '75%',
-        height: 'auto',
-        borderRadius: 10,
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#6238b4',
+  },
+  loginContainer: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    width: '75%',
+    height: 'auto',
+    borderRadius: 10,
+  },
+  textLogin: {
+    color: '#6238b4',
+    marginTop: 5,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    color: '#02074c',
+  },
+  message: {
+    marginBottom: 10,
+  },
+  logo: {
+    width: '70%',
+    maxWidth: 300,
+    maxHeight: 150,
+    marginBottom: 10,
+  },
+  inputContainer: {
+    backgroundColor: '#bdaee7',
+    width: '90%',
+    height: 35,
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    borderWidth: 0.5,
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 22,
+  },
+});
 
-      },
-      textLogin: {
-        color: '#6238b4',
-        marginTop: 5,
-        fontSize: 18,
-        fontWeight: 'bold',
-      },
-      textInput :{
-        color: '#02074c'
-        
-      },
-      message:{
-        marginBottom: 10
-      },
-    logo: {
-      width: '70%',
-      maxWidth: 300,
-      maxHeight: 150,
-      marginBottom: 10,
-    },
-  })
-
-export default SignUpScreen
+export default SignUpScreen;
